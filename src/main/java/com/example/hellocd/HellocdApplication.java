2a75efdb7cab048b6a4b659d19cff25dbc514f4e
robert <robert@email.com>
2024-07-19 12:02:31 +0000

    Revert "changed the code in java file"
    This reverts commit 918cc88dbbb109a97821e4e530f114589fc3ea9e.

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
-@EnableAutoConfiguration
 public class HellocdApplication {
 
 
